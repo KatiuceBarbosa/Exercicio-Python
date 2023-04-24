0daabbca2032408f27cbfad7937a0f5e612a81a9
Katiuce Barbosa <katiucepb@gmail.com>
2023-04-24 20:36:58 -0300

    Create Exercício 11.py

---
+++ Exercício 11.py
@@ -0,0 +1,10 @@
+"""
+Programa para cálculo de perímetro com valores informados pelo usuário
+"""
+
+base = int(input("Digite o valor da base do retangulo: "))
+altura = int(input("Digite o valor da altura do retangulo: "))
+print("\n")
+perimetro = 2*base + 2*altura
+
+print("O valor do perimetro é:", perimetro)
