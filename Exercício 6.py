d12414d462887268b013238f5a619d896763ac5f
Katiuce Barbosa <katiucepb@gmail.com>
2023-04-24 20:35:24 -0300

    Create Exercício 6.py

---
+++ Exercício 6.py
@@ -0,0 +1,10 @@
+"""
+Faça um programa em python para ler os dados de um usuário: nome, telefone e endereço.
+Ao final, o programa deve imprimir na tela um relatório com os dados do usuário.
+"""
+
+nome = input("Informe o nome do usuário: ")
+telefone = input("Informe o telefone do usuário: ")
+endereço = input("Informe o endereço do usuário: ")
+print("\n")
+print("Dados do usuário: -Nome: ", nome, "-Telefone: ", telefone, "-Endereço: ", endereço)
