075631d79e41538338a742cb3817a983469453b2
Katiuce Barbosa <katiucepb@gmail.com>
2023-04-24 20:32:33 -0300

    Create Exercício 3.py

---
+++ Exercício 3.py
@@ -0,0 +1,39 @@
+"""
+3. Faça um programa em python para cadastrar três alunos. Os dados a serem armazenados são:
+nome, e-mail, matricula, senha. Ao final o programa deve imprimir na tela um relatório com os
+dados dos alunos cadastrados. Formate apropriadamente o relatório.
+"""
+
+print("Digite os dados do Aluno 1: ")
+nomeAluno1 = input("Informe o seu nome: ")
+emailAluno1 = input("Informe o seu email: ")
+matriculaAluno1 = input("Informe a sua matrícula: ")
+senhaAluno1 = input("Informe a sua senha: ")
+print("\n")
+print("Digite os dados do Aluno 2: ")
+nomeAluno2 = input("Informe o seu nome: ")
+emailAluno2 = input("Informe o seu email: ")
+matriculaAluno2 = int(input("Informe a sua matrícula: "))
+senhaAluno2 = input("Informe a sua senha: ")
+print("\n")
+print("Digite os dados do Aluno 3: ")
+nomeAluno3 = input("Informe o seu nome: ")
+emailAluno3 = input("Informe o seu email: ")
+matriculaAluno3 = int(input("Informe a sua matrícula: "))
+senhaAluno3 = input("Informe a sua senha: ")
+print("\n")
+print("Digite os dados do Aluno 4: ")
+nomeAluno4 = input("Informe o seu nome: ")
+emailAluno4 = input("Informe o seu email: ")
+matriculaAluno4 = int(input("Informe a sua matrícula: "))
+senhaAluno4 = input("Informe a sua senha: ")
+print("\n")
+print("Dados cadastrados com sucesso!")
+print("\n")
+print("Relatório de cadastro: ")
+print("\n")
+print("Aluno 1 - Nome: ", nomeAluno1, "E-mail: ", emailAluno1, "Matrícula: ", matriculaAluno1, "Senha: ", senhaAluno1)
+print("Aluno 2 - Nome: ", nomeAluno2, "E-mail: ", emailAluno2, "Matrícula: ", matriculaAluno2, "Senha: ", senhaAluno3)
+print("Aluno 3 - Nome: ", nomeAluno3, "E-mail: ", emailAluno3, "Matrícula: ", matriculaAluno3, "Senha: ", senhaAluno3)
+print("Aluno 4 - Nome: ", nomeAluno4, "E-mail: ", emailAluno4, "Matrícula: ", matriculaAluno4, "Senha: ", senhaAluno4)
+
