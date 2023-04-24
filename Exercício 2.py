e26b32db3aa05db7e86a07d37fa84bc795a75e54
Katiuce Barbosa <katiucepb@gmail.com>
2023-04-24 20:17:49 -0300

    Create Exercício 2.py

---
+++ Exercício 2.py
@@ -0,0 +1,18 @@
+"""
+Faça um programa em python para que um usuário informe os dados de cadastro: nome, email, login e senha.
+Ao final o programa deve imprimir na tela uma mensagem informando que os dados foram cadastrados com
+sucesso no banco de dados e um relatório com dados do usuário.
+"""
+
+nome = input("Informe o seu nome: ")
+email = input("Informe o seu email: ")
+login = input("Informe o seu login: ")
+senha = input("Informe a sua senha: ")
+print("\n")
+print("Os dados foram cadastrados com sucesso!")
+print("\n")
+print("Dados cadastrados: ")
+print("Nome: ", nome)
+print("Email: ", email)
+print("Login: ", login)
+print("Senha: ", senha)
