0050ca61767f2db6521e048294ea6d6fc848857d
Katiuce Barbosa <katiucepb@gmail.com>
2023-04-24 19:49:05 -0300

    Create Exercicio 1.py

---
+++ Exercicio 1.py
@@ -0,0 +1,18 @@
+"""
+Faça um programa em python para que um usuário informe três números inteiros. Ao final o
+programa deve imprimir na tela os números informados e o resultado das seguintes operações
+algébricas: soma, subtração e multiplicação.
+"""
+
+a = int(input("Digite o valor da primeira variável: "))
+b = int(input("Digite o valor da segunda variável: "))
+c = int(input("Digite o valor da terceira variável: "))
+print("\n")
+soma = a + b + c
+subtracao = a - b - c
+multiplicacao = a * b * c
+
+print("O valor da soma: ", soma)
+print("O valor da subtracao: ", subtracao)
+print("O valor da multiplicacao: ", multiplicacao)
+
