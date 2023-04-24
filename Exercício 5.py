1af93912402975bf0d29fa513ba1dcf3b16aa889
Katiuce Barbosa <katiucepb@gmail.com>
2023-04-24 20:34:58 -0300

    Create Exercício 5.py

---
+++ Exercício 5.py
@@ -0,0 +1,14 @@
+"""
+Faça um programa em python no qual o usuário informa um número inteiro e o programa
+exibe como saída seu antecessor e seu sucessor.
+"""
+
+numeroInteiro = int(input("Digite o número inteiro: "))
+
+antecessor = numeroInteiro - 1
+sucessor = numeroInteiro + 1
+print("\n")
+print("Número informado: ", numeroInteiro)
+print("O número antecessor é: ", antecessor)
+print("O número sucessor é: ", sucessor)
+
